c388f4294d027688617f5fb28bb9f131b7a0c16b
ibiscum <ulf.luecking@gmail.com>
2024-11-23 10:24:41 +0100

    restructure

--- 7-template-programming-in-go/grpcExample/client/main.go
+++ 7-template-programming-in-go/grpcExample/client/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	pb "github.com/ibiscum/High-Performance-With-Go/7-template-programming-in-go/grpcExample/userinfo/userinfo"
+	pb "github.com/ibiscum/High-Performance-With-Go/7-template-programming-in-go/grpcExample/userinfo"
 	"google.golang.org/grpc"
 )
 
